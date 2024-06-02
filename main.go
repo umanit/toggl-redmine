528bccff8503e37e803ebdb27149e983a074e1d6
Vivien DE BONA <vdebona@umanit.fr>
2024-06-02 19:36:12 +0200

    feat: ajout du nom et l’icône pour Linux

--- main.go
+++ main.go
@@ -8,20 +8,30 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
-//go:embed all:frontend/dist
+//go:embed all:frontend/dist build/appicon.png
 var assets embed.FS
 
+const (
+	appName = "toggl track - Redmine"
+)
+
 func main() {
+	icon, err := assets.ReadFile("build/appicon.png")
+	if err != nil {
+		panic(err)
+	}
+
 	appcfg.CreateAppDir()
 
 	// Create an instance of the app structure
 	app := NewApp()
 
 	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "toggl track - Redmine",
+	err = wails.Run(&options.App{
+		Title:  appName,
 		Width:  800,
 		Height: 770,
 		AssetServer: &assetserver.Options{
@@ -33,6 +43,10 @@ func main() {
 			app,
 		},
 		Logger: logger.Create(),
+		Linux: &linux.Options{
+			ProgramName: appName,
+			Icon:        icon,
+		},
 	})
 
 	if err != nil {
